b186f66c299806d115267ea2e3df58838e298eba
尾原徳泰 <nori.tukareta@gmail.com>
2022-06-03 02:13:38 +0900

    uiの実装

--- RxSwiftFirebaseSampleApp/RxSwiftFirebaseSampleApp/ViewController.swift
+++ RxSwiftFirebaseSampleApp/RxSwiftFirebaseSampleApp/ViewController.swift
@@ -9,10 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: - ui
+    @IBOutlet weak var setButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    @IBOutlet weak var realtimeSwich: UISwitch!
+    @IBOutlet weak var upTextField: UITextField!
+    @IBOutlet weak var downTextField: UITextField!
+    
+    // MARK: - 変数
+    
+    // MARK: - ライフサイクル
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: - 関数
 
 
 }
