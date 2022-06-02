0c27113929415d20a9e81f6eedafa22b3bce61e0
尾原徳泰 <nori.tukareta@gmail.com>
2022-06-03 00:01:31 +0900

    firebase RxSwift RxCocoaインストール

---
+++ RxSwiftFirebaseSampleApp/Pods/Target Support Files/Pods-RxSwiftFirebaseSampleApp/Pods-RxSwiftFirebaseSampleApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftFirebaseSampleApp : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftFirebaseSampleApp
+@end
---
+++ RxSwiftFirebaseSampleApp/Pods/Target Support Files/Pods-RxSwiftFirebaseSampleAppTests/Pods-RxSwiftFirebaseSampleAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwiftFirebaseSampleAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwiftFirebaseSampleAppTests
+@end
